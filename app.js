518e58ea923d9e57aea41aa2944d94aa08221e7c
GorgeousStar <innocentblesel@gmail.com>
2023-01-21 22:30:12 +0100

    add some javascript to make the ham icon functional

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const navMenu = document.querySelector(".nav-menu");
 
         hamcontainer.addEventListener("click", () => {
             hamcontainer.classList.toggle("animate");
-            navMenu.classList.toggle("animate");
-           
+            navMenu.classList.toggle("animate");  
         })
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamcontainer.classList.toggle("animate");
+    navMenu.classList.toggle("animate"); 
+}))
